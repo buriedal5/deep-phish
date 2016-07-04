f19a62969660b92a1ff18ac2b97c7c94e63ad8f1
Thom Nelson <thomnelson@gmail.com>
2016-07-04 11:53:18 -0700

    extract code corresponding to setlists commit

--- extract.js
+++ extract.js
@@ -8,23 +8,38 @@ for (var i = 0; i < newSetlists.length; ++i) {
     continue;
   }
   var date = newSetlists[i].getElementsByClassName("sldate");
-  html += date[0].innerHTML.substring(date[0].innerHTML.indexOf(",") + 1) + "<br>";
+  var showHtml = "";
+  showHtml += date[0].innerHTML.substring(date[0].innerHTML.indexOf(",") + 1) + "<br>";
   var paras = newSetlists[i].getElementsByTagName("P");
+  var hasSong = false;
   for (var j = 0; j < paras.length; ++j) {
     var titles = paras[j].getElementsByTagName("B");
     if (typeof titles[0] != "undefined") {
-      html += titles[0].innerHTML + "<br>";
-      var songs = paras[j].getElementsByTagName("A");
-      for (var k = 0; k < songs.length; ++k) {
-        if (songs[k].classList.length == 0 || songs[k].classList.contains("full")) {
-          html += songs[k].href + "<br>";
+      showHtml += titles[0].innerHTML + "<br>";
+      for (var k = 0; k < paras[j].childNodes.length; ++k) {
+        var node = paras[j].childNodes[k];
+        if (node.nodeType == 1
+            && node.tagName == "A"
+            && (node.classList.length == 0
+            || node.classList.contains("full"))) {
+          hasSong = true;
+          showHtml += node.href + "<br>";
+        } else if (node.nodeType == 3) {
+          if (node.nodeValue.startsWith(" >")) {
+            showHtml += "&lt;into&gt;<br>";
+          } else if (node.nodeValue.startsWith(" ->")) {
+            showHtml += "&lt;segue&gt;<br>";
+          }
         }
       }
     }
+  }
+  if (hasSong) {
+    html += showHtml;
+    html += "<br>";
   }
 }
 var elem = document.createElement("DIV");
 elem.id = "extracted";
 elem.style.marginTop = "500px";
-elem.innerHTML = html;
-document.body.appendChild(elem);
+elem.innerHTML = html; document.body.appendChild(elem);
