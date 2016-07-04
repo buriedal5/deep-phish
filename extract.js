90e1cb7c85f00d584ae18af319a8d6dec041bbe3
buriedal5 <thomnelson@gmail.com>
2016-07-04 10:36:00 -0700

    Create extract.js

---
+++ extract.js
@@ -0,0 +1,25 @@
+var newSetlists = document.getElementsByClassName("setlist");
+var html = "";
+for (var i = 0; i < newSetlists.length; ++i) {
+  if (newSetlists[i].classList.contains("excluded")) {
+    continue;
+  }
+  var date = newSetlists[i].getElementsByClassName("sldate");
+  html += date[0].innerHTML.substring(date[0].innerHTML.indexOf(",") + 1) + "<br>";
+  var paras = newSetlists[i].getElementsByTagName("P");
+  for (var j = 0; j < paras.length; ++j) {
+    var titles = paras[j].getElementsByTagName("B");
+    if (typeof titles[0] != "undefined") {
+      html += titles[0].innerHTML + "<br>";
+      var songs = paras[j].getElementsByTagName("A");
+      for (var k = 0; k < songs.length; ++k) {
+        if (songs[k].classList.length == 0 || songs[k].classList.contains("full")) {
+          html += songs[k].href + "<br>";
+        }
+      }
+    }
+  }
+}
+var elem = document.createElement("DIV");
+elem.style.marginTop = "100px";
+elem.innerHTML = html; document.body.appendChild(elem);
